e60d21d44bd292c0fecec4fef4d676f695c30eb7
kam2k-dev <scream11kam@gmail.com>
2026-08-12 06:17:56 +0700

    fix: type error for PORT variable

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import { waAuthService } from "./server/auth/waAuth";
 
 async function startServer() {
   const app = express();
-  const PORT = process.env.PORT || 5000;
+  const PORT = Number(process.env.PORT) || 5000;
 
   app.use(express.json());
 
